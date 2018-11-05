869832003a5272434adc33a2568c1220d566587e
Day <874288011@qq.com>
2018-11-06 00:06:24 +0800

    HTML 引入 bundle

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', 'src/index.js'),
@@ -14,7 +15,21 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.html$/,
+        exclude: /public/,
+        use: {
+          loader: 'html-loader'
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'zero to one',
+      template: path.resolve(__dirname, '..', 'public/template.html'),
+      filename: path.resolve(__dirname, '..', 'dist/index.html')
+    })
+  ]
 };
